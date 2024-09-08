027791b8ceb8705d6d089964fb03f972afed8c0f
Rosy122333 <80847580+Rosy122333@users.noreply.github.com>
2024-09-07 20:59:58 -0400

    ollama model working

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #!/bin/python
 from flask import Flask, render_template, request, render_template_string
-# import requests
+from langchain_ollama import OllamaLLM
+import requests
 import json
 app = Flask(__name__)
-
+model = OllamaLLM(model="llama3:latest")
+message = ""
 @app.route('/')
 def index():
     return render_template("index.html") ##would need a template folder and then need index.html in there and then this works
@@ -11,7 +13,9 @@ def index():
 @app.route('/submit', methods=['POST'])
 def submit():
     textbox_value = request.form.get('textbox')
-    return f'You submitted: {textbox_value}'
+    message = textbox_value
+    result = model.invoke(input=message)
+    return f'Ollama returned: {result}'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0',port=80)
