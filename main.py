38c3dfd2e97d43b1ad1ebe8115b436ca0078bc22
Ronit Kapoor <ronitkapoor@Ronits-MacBook-Air-3.local>
2024-09-09 21:01:23 -0400

    changed port from 80 to 3000

--- main.py
+++ main.py
@@ -19,4 +19,4 @@ def submit():
     # return f'Ollama returned: {result}'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0',port=80)
+    app.run(debug=True, host='0.0.0.0',port=3000)
